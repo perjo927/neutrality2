04adf35dbbf856730c307f63074a0263633d9ea5
perjo927 <perjo927@student.liu.se>
2015-09-28 19:31:46 +0200

    Updated soundcloud

--- pwaon/server/view_models/resources.vm.js
+++ pwaon/server/view_models/resources.vm.js
@@ -16,16 +16,14 @@ var resources3 = {
     icon: "mdi-av-volume-up",
     title: "Free Audio",
     alt: "Audio",
-    link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/223087324&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
-    // TODO: Link to a soundset directly name free resources directly related to the website content
+    link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/149510143&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
 };
 
 var resources4 = {
     icon: "mdi-action-question-answer",
     title: "OTH Audio FAQs",
     alt: "Audio FAQ",
-    link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/223087324&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
-    // TODO: Link to a soundset directly name free resources directly related to the website content
+    link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/149510254&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
 };
 
 VM.sections["resources"] = [
