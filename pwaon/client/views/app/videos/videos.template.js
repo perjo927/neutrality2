5ad1f83642684f85d93ef7e3f74c1c89985a97f3
perjo927 <perjo927@student.liu.se>
2015-11-23 21:01:35 +0100

    More with videos

--- pwaon/client/views/app/videos/videos.template.js
+++ pwaon/client/views/app/videos/videos.template.js
@@ -1,7 +1,12 @@
 // TODO: show staggered list
 
 Template.videos.onRendered(() => {
+    $('select').material_select();
 });
 
 Template.video_list.onRendered(() => {
+});
+
+Template.videos_search.events({
+    'click .videos-search': () => App.Template.Jquery.focus({_id: "videos-search"})
 });
